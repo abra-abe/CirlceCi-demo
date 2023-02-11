aa7082490326bb0b1135747e975608a4261a48af
abra-abe <abewills17@gmail.com>
2023-02-11 15:55:10 +0300

    Second commit added homepaage

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+//Api endpoint that gets the home page
+app.get('/', (req, res) => {
+    res.send("This is the home page");
+})
 
 app.listen(5000, () => {
     console.log("Server is listening on port 5000");
