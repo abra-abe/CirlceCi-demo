68e02ee673874ecf9d59085d7c1d68f22250effc
abra-abe <abewills17@gmail.com>
2023-02-13 18:50:42 +0300

    Added: ejs template engine index.ejs

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 
+//setting up the view engine
+app.set('view-engine', 'ejs');
+
 //Api endpoint that gets the home page
 app.get('/', (req, res) => {
-    res.send("This is the home page");
+    res.render('index.js')
 })
 
 module.exports = app
